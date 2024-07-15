94349fb08331225a0596f6386b1ded9e35d5cc8e
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-15 17:29:17 +0300

    Fixed sticking footer to bottom of page

--- nextjs-app/src/app/page.js
+++ nextjs-app/src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className={styles.wrapper}>
             <Header />
-            <main className={styles.contacts}>
+            <main className={styles.main}>
                 <Container>
                     <p>main content</p>
                 </Container>
