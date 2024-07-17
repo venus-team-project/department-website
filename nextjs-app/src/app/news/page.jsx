4b244ac40339267b58e56eac6732a6540f03623a
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-17 10:55:30 +0300

    Added empty pages for all routes

--- nextjs-app/src/app/news/page.jsx
+++ nextjs-app/src/app/news/page.jsx
@@ -3,7 +3,7 @@ import Container from '@/components/Container/Container'
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 
-export default function Science() {
+export default function News() {
     return (
         <div className={styles.wrapper}>
             <Header />
