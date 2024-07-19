f054b2ab4cdc80b98f32cc91a23e9503f94d5b0c
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-07-19 20:36:38 +0300

    Deleted code for api testing

--- nextjs-app/src/app/contacts/page.jsx
+++ nextjs-app/src/app/contacts/page.jsx
@@ -1,31 +1,9 @@
-'use client'
-
 import styles from './page.module.scss'
 import Container from '@/components/Container/Container'
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
-import { useEffect, useState } from 'react'
 
 export default function Contacts() {
-    const [posts, setPosts] = useState([])
-
-    const loadPosts = async () => {
-        try {
-            const responsePosts = await fetch(
-                'https://department-website.bulhakov.dev/api/db/books/list'
-            )
-            const newPosts = await responsePosts.json()
-
-            setPosts((prevPosts) => [...prevPosts, ...newPosts])
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    useEffect(() => {
-        loadPosts()
-    }, [])
-
     return (
         <div className={styles.wrapper}>
             <Header />
