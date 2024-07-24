fc3249dbf8447bfe152de65bd7fff6d717427d17
manni-ukr <mykhailova777@gmail.com>
2024-07-24 19:42:53 +0300

    Update changelog-init-02.sql (#35)

--- be/teachers-db-rest-demo/src/main/resources/db/changelog/changes/changelog-init-02.sql
+++ be/teachers-db-rest-demo/src/main/resources/db/changelog/changes/changelog-init-02.sql
@@ -1,5 +1,21 @@
 --changeset liquibase-docs:insert-example
-INSERT INTO books VALUES(1, 'BookDto 1', 'Annotation 1');
-INSERT INTO books VALUES(2, 'BookDto 2', 'Annotation 2');
-INSERT INTO books VALUES(3, 'BookDto 3', 'Annotation 3');
-INSERT INTO books VALUES(4, 'BookDto 4', 'Annotation 4');
+INSERT INTO authors VALUES(default,'Іван', 'Ткаченко');
+INSERT INTO authors VALUES(default,'Марія', 'Мельник');
+INSERT INTO authors VALUES(default,'Євген', 'Кравченко');
+INSERT INTO authors VALUES(default,'Андрій', 'Литвин');
+
+-- changeset liquibase:8
+INSERT INTO categories VALUES(default,'Бази даних');
+INSERT INTO categories VALUES(default,'Штучний інтелект');
+INSERT INTO categories VALUES(default,'Тестування');
+INSERT INTO categories VALUES(default,'Комп`ютерні мережі');
+
+-- changeset liquibase:9
+INSERT INTO books (id, author_id, data, category_id, title, annotation, pdf_url) VALUES
+(default, 1, '2024-07-01', 2, 'Використання штучного інтелекту', 'штучний інтелект', null);
+INSERT INTO books (id, author_id, data, category_id, title, annotation, pdf_url) VALUES
+(default, 1, '2024-07-03', 1, 'Структура бази даних', 'дані, бази даних', null);
+INSERT INTO books (id, author_id, data, category_id, title, annotation, pdf_url) VALUES
+(default, 3, '2024-07-05', 3, 'Автоматизоване тестування', 'тест-кейс', null);
+INSERT INTO books (id, author_id, data, category_id, title, annotation, pdf_url) VALUES
+(default, 4, '2024-07-08', 4, 'Мережеві протоколи', 'мережа, протокол, рівні протоколів', null);
