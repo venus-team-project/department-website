7cb1fd635615d33c213c47c63da7477dac93aba4
manni-ukr <mykhailova777@gmail.com>
2024-07-24 20:20:46 +0300

    Update Book.java (#43)

--- be/teachers-db-rest-demo/src/main/java/com/example/teachers_db_demo/model/Book.java
+++ be/teachers-db-rest-demo/src/main/java/com/example/teachers_db_demo/model/Book.java
@@ -23,14 +23,85 @@ import lombok.ToString;
 @Table(name="books")
 public class Book {
 
-  @Id
-  @GeneratedValue
-  @Column(name = "id")
+ @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "title")
+  @ManyToOne
+  @JoinColumn(name = "author_id", nullable = false)
+  private Author author;
+
+  @Column(name = "data", nullable = false)
+  @Temporal(TemporalType.DATE)
+  private Date data;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id", nullable = false)
+  private Category category;
+
+  @Column(name = "title", nullable = false, length = 150)
   private String title;
 
-  @Column(name = "annotation")
+  @Column(name = "annotation", nullable = false, length = 255)
   private String annotation;
+
+  @Column(name = "pdf_url")
+  private String pdfUrl;
+
+  // Getters and setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
+  }
+
+  public Date getData() {
+    return data;
+  }
+
+  public void setData(Date data) {
+    this.data = data;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAnnotation() {
+    return annotation;
+  }
+
+  public void setAnnotation(String annotation) {
+    this.annotation = annotation;
+  }
+
+  public String getPdfUrl() {
+    return pdfUrl;
+  }
+
+  public void setPdfUrl(String pdfUrl) {
+    this.pdfUrl = pdfUrl;
+  }
 }
